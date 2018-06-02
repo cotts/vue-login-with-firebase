c2b86f8586aa39cebf694e16020f51f0136a4212
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 18:12:49 -0300

    configurated more routes and auth method

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import firebase from 'firebase';
 
 import Home from '@/components/Home';
 import Login from '@/components/Login';
+import Cadastro from '@/components/Cadastro';
+import Index from '@/components/Index';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -19,5 +22,28 @@ export default new Router({
       name: 'Login',
       component: Login,
     },
+    {
+      path: '/cadastro',
+      name: 'Cadastro',
+      component: Cadastro,
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: Index,
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(info => info.meta.requiresAuth);
+  if (requiresAuth && !currentUser) next('/');
+  else if (!requiresAuth && currentUser) next('index');
+  else next();
+});
+
+export default router;
