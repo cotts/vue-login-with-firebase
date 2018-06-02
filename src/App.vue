619484f08b229c5ef3d825215de435826e65b41f
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 14:15:10 -0300

    initial templates

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h2>App</h2>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+};
+</script>
