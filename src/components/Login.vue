581868328d35d622ab7a9891241f1786c80885a1
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 13:19:06 -0300

    creating login page

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h3>Login</h3>
+    <input type="text">
+    <input type="password">
+    <button>Entrar</button>
+    <p>Ainda não tem uma conta? Você pode criar uma</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
