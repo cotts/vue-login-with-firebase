0119781203264d03dafabe4b04e3d3e4450cda49
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 14:12:55 -0300

    configured firebase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 import Vue from 'vue';
-import App from '@/App';
+import firebase from 'firebase';
 
-import router from '@/router';
+import App from './App';
+import router from './router';
 
 Vue.config.productionTip = false;
 
+const config = {
+  apiKey: 'AIzaSyAdHLux_W_MbMyw_388kNIbb78d48hi324',
+  authDomain: 'vue-login-with-firebase.firebaseapp.com',
+  databaseURL: 'https://vue-login-with-firebase.firebaseio.com',
+  projectId: 'vue-login-with-firebase',
+  storageBucket: '',
+  messagingSenderId: '74051838231',
+};
+
+firebase.initializeApp(config);
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: <App />,
   components: { App },
+  template: '<App/>',
 });
