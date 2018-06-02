0f01c250c50e364cd30cafa4720472a58f13351f
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 18:36:27 -0300

    logout button

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <p @click="logout">Logout</p>
     <h2>Index</h2>
   </div>
 </template>
+<script>
+import firebase from 'firebase';
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    logout: function() {
+      const that = this;
+      firebase
+        .auth()
+        .signOut()
+        .then(() => that.$router.replace('/'));
+    },
+  },
+};
+</script>
+
