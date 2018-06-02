229cd7b2e5bc668ee060572789ab19d664244225
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 14:13:50 -0300

    removed hash from url

--- src/router.js
+++ src/router.js
@@ -7,8 +7,10 @@ import Login from '@/components/Login';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-    { path: '/',
+    {
+      path: '/',
       name: 'Home',
       component: Home,
     },
