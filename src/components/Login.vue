75af2ba693afcd49771d3ef8a650a5f68014960d
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-02 18:36:16 -0300

    configured login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="login">
     <h3>Login</h3>
-    <input type="text">
-    <input type="password">
-    <button>Entrar</button>
+    <input type="text" v-model="email">
+    <input type="password" v-model="password">
+    <button @click="signIn">Entrar</button>
+    <p> {{ mensagem }}</p>
     <p>Ainda não tem uma conta? Você pode criar uma</p>
   </div>
 </template>
 <script>
+import firebase from 'firebase';
 export default {
-  name: "login",
+  name: 'login',
   data() {
-    return {};
+    return {
+      email: '',
+      password: '',
+      mensagem: '',
+    };
   },
-  methods: {}
+  methods: {
+    signIn: function() {
+      const that = this;
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(that.email, that.password)
+        .then(user => {
+          alert('Usuário Conectado!');
+          setTimeout(function() {
+            that.$router.go('/index');
+          }, 1000);
+        }, err => (that.mensagem = 'Usuário ou senha incorretos!'));
+    },
+  },
 };
 </script>
