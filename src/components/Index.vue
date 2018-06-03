5af30422fd222c92d3ea13e9afed87371392dd89
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-03 03:59:58 -0300

    updated videos interaction

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="main container-fluid">
-    <h2 class=title> Videos de Treinamento</h2>
+    <h2 class=title>Videos de Treinamento</h2>
+        <div class="player">
+          <div class="video" v-bind:class="[{ activated : isSelected }, {stoped : isStoped }]" v-html="playVideo">
+          </div>
+          <h4><span class="badge badge-secondary btn-close" @click="closeVideo" v-show="isSelected">Fechar Vídeo</span></h4>
+        </div>
     <div class="content">
       <div class="card" v-for="(video, id) in videos">
-        <img class="card-img-top" v-bind:src="'http://img.youtube.com/vi/' +video+ '/hqdefault.jpg'">
+        <img class="card-img-top" v-bind:src="'http://img.youtube.com/vi/' +video+ '/hqdefault.jpg'" @click="openVideo(video)">
       </div>
 
     </div>
@@ -21,7 +26,22 @@ export default {
         'ek_RKOAlctg',
         'n8eW8D04tNc',
       ],
+      isSelected: false,
+      isStoped: true,
+      playVideo: '',
     };
+  },
+  methods: {
+    openVideo: function(video) {
+      this.isSelected = true;
+      this.isStoped = false;
+      this.playVideo = `<iframe width="640" height="440" src="http://www.youtube.com/embed/${video}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+    },
+    closeVideo: function() {
+      this.isStoped = true;
+      this.isSelected = false;
+      this.playVideo = '';
+    },
   },
 };
 </script>
@@ -35,16 +55,33 @@ export default {
 }
 
 .content {
+  padding-top: 30px;
   width: 90%;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-around;
   margin: 0 auto;
 }
-
 .card {
-  width: 250px;
+  width: 200px;
   margin: 20px;
+}
+.stoped {
+  display: none;
+}
+.player {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+.player .video {
+  margin: 20px auto;
+}
+
+.btn-close {
+  margin-right: 200px;
+  cursor: pointer;
+  margin: 0 auto;
 }
 </style>
 
