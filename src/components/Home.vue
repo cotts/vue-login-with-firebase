38d410c749516f1502433c12da7a98d97663ca2c
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-03 01:23:41 -0300

    finished Home view

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h4>Home</h4>
+  <div class="main container-fluid">
+    <h2 class="title">Bem-vindo ao portal de Videos Online</h2>
+    <h3 class="title">Escolha entre as opções no canto superior direito</h3>
+
   </div>
 </template>
 <script>
@@ -11,3 +13,14 @@ export default {
   },
 };
 </script>
+<style scoped>
+.title {
+  font-family: 'Sunflower', sans-serif;
+  text-align: center;
+}
+.title:first-child {
+  margin-top: 100px;
+  margin-bottom: 20px;
+}
+</style>
+
