12afccfdf04653de0ec34a15b8ba5b7d04680eb9
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-03 01:23:09 -0300

    finished home view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,79 @@
 <template>
-  <div id="app">
-    <h2>App</h2>
+  <div id="app" class="container-fluid">
+    <nav class="navbar navbar-expand-lg">
+      <a class="navbar-brand">Video Instructor</a>
+      <div class="nav-menu" v-show="userMenu">
+        <ul class="nav justify-content-end">
+          <li class="nav-item active">
+            <router-link to="/profile" class="nav-link">Meus Dados</router-link>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" @click="logout">Sair</a>
+          </li>
+        </ul>
+      </div>
+      <div class="nav-menu" v-show="!userMenu">
+        <ul class="nav justify-content-end">
+          <li class="nav-item active">
+            <router-link to="/cadastro" class="nav-link">Cadastrar</router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link to="/login" class="nav-link" v>Entrar</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
 <script>
+import firebase from 'firebase';
 export default {
   name: 'App',
   data() {
-    return {};
+    return {
+      userMenu: false,
+    };
+  },
+  methods: {
+    logout: function() {
+      const that = this;
+      firebase
+        .auth()
+        .signOut()
+        .then(() => that.$router.go('/'));
+    },
+  },
+  mounted() {
+    if (firebase.auth().currentUser) {
+      this.userMenu = true;
+    }
   },
 };
 </script>
+<style scoped>
+a:link,
+a:active,
+a:visited,
+a:hover {
+  text-decoration: none;
+  color: #fff;
+}
+.container-fluid {
+  background-color: #f1f5fa;
+  height: 100vh;
+  padding: 0;
+}
+.navbar {
+  color: #fff;
+  background-color: #132266;
+  font-family: 'Sunflower', sans-serif;
+  justify-content: space-between;
+}
+.nav-menu {
+  float: right;
+}
+.nav-link {
+  cursor: pointer;
+}
+</style>
