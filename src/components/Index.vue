e063237115aabf491100d969c2d3f1ac5f41ee24
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-03 01:24:20 -0300

    moved logout button to app.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,24 +1,9 @@
 <template>
   <div>
-    <p @click="logout">Logout</p>
+
     <h2>Index</h2>
   </div>
 </template>
 <script>
-import firebase from 'firebase';
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    logout: function() {
-      const that = this;
-      firebase
-        .auth()
-        .signOut()
-        .then(() => that.$router.replace('/'));
-    },
-  },
-};
 </script>
 
