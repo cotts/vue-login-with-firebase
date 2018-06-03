a38513ce6177ba9b012f1a4e43b49bd9441b2de6
Thadeu Cotts <thadeu.cotts@gmail.com>
2018-06-03 01:24:31 -0300

    finished login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="login">
-    <h3>Login</h3>
-    <input type="text" v-model="email">
-    <input type="password" v-model="password">
-    <button @click="signIn">Entrar</button>
-    <p> {{ mensagem }}</p>
-    <p>Ainda não tem uma conta? Você pode criar uma</p>
+    <h3 class="title">Login</h3>
+    <input class="form-control" type="text" v-model="email">
+    <input class="form-control" type="password" v-model="password">
+    <button class="btn btn-primary" @click="signIn">Entrar</button>
+    <p class="mensagem">Ainda não tem uma conta? Crie a sua conta <router-link to="cadastro">aqui</router-link></p>
   </div>
 </template>
 <script>
 import firebase from 'firebase';
+
 export default {
   name: 'login',
   data() {
     return {
       email: '',
       password: '',
-      mensagem: '',
     };
   },
   methods: {
@@ -25,13 +24,37 @@ export default {
       firebase
         .auth()
         .signInWithEmailAndPassword(that.email, that.password)
-        .then(user => {
-          alert('Usuário Conectado!');
-          setTimeout(function() {
-            that.$router.go('/index');
-          }, 1000);
-        }, err => (that.mensagem = 'Usuário ou senha incorretos!'));
+        .then(
+          user => {
+            that.$toaster.info('Usuário logado!');
+            setTimeout(function() {
+              that.$router.go('/index');
+            }, 1000);
+          },
+          err => that.$toaster.error('Usuário ou senha incorretos')
+        );
     },
   },
 };
 </script>
+<style scoped>
+.login {
+  font-family: 'Sunflower', sans-serif;
+  width: 500px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 60px;
+}
+
+.title {
+  margin-bottom: 40px;
+}
+.form-control {
+  margin-bottom: 20px;
+}
+
+.mensagem {
+  margin-top: 30px;
+}
+</style>
+
